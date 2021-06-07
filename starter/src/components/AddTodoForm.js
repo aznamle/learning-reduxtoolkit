cdaf66a198e8ccb022ade709dc5adc6137081209
aznamle <namle1998@hotmail.com>
2021-06-07 11:51:23 -0700

    finished starter todo app

--- starter/src/components/AddTodoForm.js
+++ starter/src/components/AddTodoForm.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../redux/todoSlice'
 
 const AddTodoForm = () => {
 	const [value, setValue] = useState('');
 
+	//usedispatch to send data to store
+	const dispatch = useDispatch()
+
 	const onSubmit = (event) => {
 		event.preventDefault();
-		console.log('user entered: ' + value);
+		dispatch(addTodo({
+			title: value,
+		}))
 	};
 
 	return (
--- starter/src/components/TodoList.js
+++ starter/src/components/TodoList.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { useSelector } from 'react-redux'
 
 const TodoList = () => {
-	const todos = [
-		{ id: 1, title: 'todo1', completed: false },
-		{ id: 2, title: 'todo2', completed: false },
-		{ id: 3, title: 'todo3', completed: true },
-		{ id: 4, title: 'todo4', completed: false },
-		{ id: 5, title: 'todo5', completed: false },
-	];
+
+	//useselector to grab all data in todos state from store
+	const todos = useSelector((state)=> state.todos)
 
 	return (
 		<ul className='list-group'>
